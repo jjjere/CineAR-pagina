e05b40bd7e5f40a47397a3247bb9b9c8525d3d92
Fabioexemaciel <76603662+jjjere@users.noreply.github.com>
2021-09-29 13:59:04 -0600

    trabajo terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,29 +2,32 @@ import React from "react";
 import "./styles/Footer.css";
 const Footer = () => {
   return (
-    <footer>
-      <h6>Todos los derechos reservados 2003-2021. República Argentina. INCAA<br/>
-      Lima 319; Código Postal: C1073AAG, CABA; República Argentina.</h6>
-     <div className="Footer">
-      <ul>
-        <li>
-          <a href="http://cine.ar/">
-            <img src="http://cine.ar/img/logo-cinear.svg" alt="" />
-          </a>
-        </li>
+    <footer className="Footer">
+      <h6>
+        Todos los derechos reservados 2003-2021. República Argentina. INCAA
+        <br />
+        Lima 319; Código Postal: C1073AAG, CABA; República Argentina.
+      </h6>
+      <div>
+        <ul>
+          <li>
+            <a href="http://cine.ar/">
+              <img src="http://cine.ar/img/logo-cinear.svg" alt="" />
+            </a>
+          </li>
 
-        <li>
-          <a href="https://www.arsat.com.ar/">
-            <img src="http://cine.ar/img/logo-arsat.svg" alt="" />
-          </a>
-        </li>
+          <li>
+            <a href="https://www.arsat.com.ar/">
+              <img src="http://cine.ar/img/logo-arsat.svg" alt="" />
+            </a>
+          </li>
 
-        <li>
-          <a href="http://www.incaa.gob.ar/">
-            <img src="http://cine.ar/img/logo-incaa.svg" alt="" />
-          </a>
-        </li>
-      </ul>
+          <li>
+            <a href="http://www.incaa.gob.ar/">
+              <img src="http://cine.ar/img/logo-incaa.svg" alt="" />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
